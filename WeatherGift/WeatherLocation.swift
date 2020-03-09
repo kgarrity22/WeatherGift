cdc45a76b6d3e84cdf19d2068b9bc26803bd1faf
Kathleen Garrity <garrityk@bc.edu>
2020-03-09 14:18:07 -0400

    7.2 setting up WeatherGift ViewController

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Kathleen Garrity on 3/9/20.
+//  Copyright © 2020 Kathleen Garrity. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
