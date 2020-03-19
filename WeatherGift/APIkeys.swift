b7b36fae7f0b3308677d83f1d625f04098e96984
Kathleen Garrity <garrityk@bc.edu>
2020-03-19 16:45:14 -0400

    adding file for access to google places functionality (hopefully file is ignored)

---
+++ WeatherGift/APIkeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIkeys.swift
+//  WeatherGift
+//
+//  Created by Kathleen Garrity on 3/19/20.
+//  Copyright © 2020 Kathleen Garrity. All rights reserved.
+//
+
+import Foundation
+
+struct APIkeys {
+    static let googlePlacesKey = "AIzaSyDWvtgYv6cLS9xavgeXHTbiKCw4s6CqHgQ"
+}
